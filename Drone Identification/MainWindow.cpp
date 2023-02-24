c1061cba1bf07930efd8c7dcc679aac23f98515c
vva07 <vva0779@yandex.ru>
2023-02-24 19:42:02 +0300

    Small progress on button slots

--- Drone Identification/MainWindow.cpp
+++ Drone Identification/MainWindow.cpp
@@ -1,6 +1,7 @@
 #include "MainWindow.h"
 #include "LogAnalyzer.h"
 #include <memory>
+#include "qfiledialog.h"
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -8,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui.setupUi(this);
     ui.verticalLayout->setAlignment(ui.startButton, Qt::AlignCenter);
+    ui.statusBar->showMessage("Choose input file before starting! Default output is <CWD>/output.txt");
 }
 
 void MainWindow::startButtonClicked()
@@ -23,16 +25,23 @@ void MainWindow::startButtonClicked()
     }
 }
 
-MainWindow::~MainWindow()
-{
-    
-
-}
-
 void MainWindow::inputFileButton()
 {
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open .log data file"), QDir::currentPath(), tr("log files (*.log)"));
+    if (!fileName.isEmpty())
+    {
+        ui.startButton->setEnabled(true);
+        ui.statusBar->showMessage("Input file was set successfully, ready to start");
+    }
 }
 
 void MainWindow::outputFileButton()
 {
+
+}
+
+MainWindow::~MainWindow()
+{
+
+
 }
