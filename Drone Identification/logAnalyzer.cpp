76c72869599a4297f1b9bbf5011186a7488ebd97
vva07 <vva0779@yandex.ru>
2023-03-17 02:52:59 +0300

    Check for beacon frames and SSID extraction draft

--- Drone Identification/logAnalyzer.cpp
+++ Drone Identification/logAnalyzer.cpp
@@ -4,28 +4,10 @@
 #include "qtextstream.h"
 #include "qcryptographichash"
 #include <memory>
-#include <tins/tins.h>
 #include <iostream>
-
-using namespace Tins;
-
 
 
 quint32 LogAnalyzer::crcTable[256] = {};
-
-void LogAnalyzer::generateCrcTable() 
-{
-    const quint32 IEEE80211_POLY = 0xedb88320;
-    for (int i = 0; i < 256; i++)
-    {
-        uint32_t c = i;
-        for (int j = 0; j < 8; j++)
-        {
-            c = (c & 1) ? (IEEE80211_POLY ^ (c >> 1)) : (c >> 1);
-        }
-        this->crcTable[i] = c;
-    }
-}
 
 bool LogAnalyzer::analizeLogs(QString logFileName)
 {
@@ -41,12 +23,21 @@ bool LogAnalyzer::analizeLogs(QString logFileName)
         }
         qInfo() << "Started input file log analysis"; 
         quint32 verifiedFrames = 0;
+        quint32 beacons = 0;
         this->generateCrcTable();
         while (!fileStream -> atEnd())
         {
             QString line = fileStream -> readLine();
-            if (checksumCRC(line))
+            std::unique_ptr<QByteArray> frame = extractFrameData(line);
+            if (checksumCRC(line))  // Refactor so it takes frameData QByteArray as arg
+            {
                 verifiedFrames += 1;
+                if (isBeaconFrame(frame))
+                {
+                    auto ssid = extractSSID(frame);
+                    beacons++;
+                }
+            }
         }
         fileptr->close();
         return true;
@@ -71,7 +62,6 @@ std::unique_ptr<QTextStream> LogAnalyzer::openFile(const QString& fileName, std:
 
 bool LogAnalyzer::checksumCRC(QString& line)
 {
-    
     if (line.isEmpty())
     {
         qWarning() << "Empty line received!";
@@ -83,35 +73,14 @@ bool LogAnalyzer::checksumCRC(QString& line)
         qWarning() << "Could not extract frame hex from frame data!";
         return false;
     }
-
     uint32_t fcs = *(reinterpret_cast<const uint32_t*>(frame -> right(fcsLen).constData()));
     QByteArray frameContent = frame->left(frame->size() - fcsLen);
-
     quint32 checksum = calculateCRC32(frameContent);
-    //uint8_t* frame_bytes = reinterpret_cast<uint8_t*>(frame -> data());
-    //std::vector<uint8_t> frame_v(frame->begin(), frame->end());
-    //PDU::serialization_type pdu_bytes(*frame_bytes);
-    //RawPDU raw_pdu(pdu_bytes);
-    //const Dot11* dot11 = raw_pdu.find_pdu<Dot11>();
-    //if (!dot11) {
-    //    qWarning() << "The packet (" << frame.get() << ") is not an IEEE802.11 frame";
-    //    return false;
-    //}
-    //
-    //std::string addr = dot11->addr1().to_string();
-    
-    /*
-    QString fcs = frameHex -> right(fcsLen);  
-    QString frameContent = frameHex -> left(frameHex -> size() - fcsLen);
-    quint32 checksum = calculateCRC32(frameContent);
-
-    return fcs.toUInt(nullptr, 16) == checksum;*/
     return checksum == fcs;
 }
 
 std::unique_ptr<QByteArray> LogAnalyzer::extractFrameData(QString& line)
 {
-    // TODO: insert return statement here
     QStringList frameData = line.split(",");
     if (frameData.size() != 5)
     {
@@ -124,6 +93,20 @@ std::unique_ptr<QByteArray> LogAnalyzer::extractFrameData(QString& line)
     return std::make_unique<QByteArray>(byteArray);
 }
 
+void LogAnalyzer::generateCrcTable()
+{
+    const quint32 IEEE80211_POLY = 0xedb88320;
+    for (int i = 0; i < 256; i++)
+    {
+        uint32_t c = i;
+        for (int j = 0; j < 8; j++)
+        {
+            c = (c & 1) ? (IEEE80211_POLY ^ (c >> 1)) : (c >> 1);
+        }
+        this->crcTable[i] = c;
+    }
+}
+
 quint32 LogAnalyzer::calculateCRC32(const QByteArray& frameContent)
 {
     quint32 crc = 0xFFFFFFFF;
@@ -131,15 +114,32 @@ quint32 LogAnalyzer::calculateCRC32(const QByteArray& frameContent)
     for (const char byte : frameContent)
         crc = (crc >> 8) ^ crcTable[(crc ^ byte) & 0xFF];
 
- 
-
     return crc ^ 0xFFFFFFFF;
 }
 
 
-bool LogAnalyzer::isBeaconFrame(void* frame)
+bool LogAnalyzer::isBeaconFrame(std::unique_ptr<QByteArray>& frame)
 {
-    return 0;
+    QByteArray controlHeader = frame->left(2);
+    quint16 ch_firstByte = controlHeader.at(0);
+    quint8 ch_secondByte = controlHeader.at(1);
+    quint8 frameType = (ch_firstByte & 0b00110000) >> 4;
+    quint8 frameSubtype = (ch_firstByte & 0b00001111);
+    bool toDs = (ch_secondByte & 0x80) == 0x80;
+    bool fromDs = (ch_secondByte & 0x40) == 0x40;
+
+    return (frameType == 0x00) && (frameSubtype == 0x08) && !toDs && !fromDs;
 }
+
+std::unique_ptr<QByteArray> LogAnalyzer::extractSSID(std::unique_ptr<QByteArray>& frame)
+{
+     quint8 id = frame->at(ssidOffset);
+     quint8 length = frame->at(ssidOffset + 1);
+     QByteArray ssidBytes;
+     if (id == 0)
+         ssidBytes = frame -> mid(ssidOffset + 2, length);
+     return std::make_unique<QByteArray>(ssidBytes);
+}
+
 
 
--- Drone Identification/logAnalyzer.h
+++ Drone Identification/logAnalyzer.h
@@ -11,12 +11,14 @@ public:
 private:
 	void generateCrcTable();
 	bool checksumCRC(QString& line);
-	bool isBeaconFrame(void* frame);
+	bool isBeaconFrame(std::unique_ptr<QByteArray>& frame);
+	std::unique_ptr<QByteArray> extractSSID(std::unique_ptr<QByteArray>& frame);
 	std::unique_ptr<QByteArray> extractFrameData(QString& line);
 	std::unique_ptr<QTextStream> openFile(const QString& fileName, std::unique_ptr<QFile>& fileptr);
 	quint32 calculateCRC32(const QByteArray& data);
 
 	const int fcsLen = 4;
+	const int ssidOffset = 36;
 	static quint32 crcTable[256];
 };
 
